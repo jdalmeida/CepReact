02ca54c8e5ac010c5073f46b5439af3c36be342a
João Gabriel de Almeida <110470218+cocacolacomvodka@users.noreply.github.com>
2023-03-23 16:10:52 +0000

    update main

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,5 @@ import { AppRoutes } from './routes'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App/>
-    <AppRoutes />
   </React.StrictMode>,
 )
